bd075c5d5bb4ee9f75b287e21956dbf96e35b5cd
Sowrov <mdsowrov315@gmail.com>
2021-09-23 02:29:17 +0300

    product added dynamicly

--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ShopContainer from '../ShopContainer/ShopContainer';
 import './Shop.css'
 const Shop = () => {
+    const [product, setProduct] = useState([])
+    useEffect(()=>{
+        fetch('./products.json')
+        .then(res=>res.json())
+        .then(data=> setProduct(data))
+    },[])
     return (
         <div className='main'>
           <div className="shop">
                 <h1>product:</h1>
+        {
+            product.map(product=><ShopContainer key={product.key} product={product}/>)
+        }
           </div>
               <div className="shop-cart">
                   <h2>order list:</h2>
--- src/component/ShopContainer/ShopContainer.js
+++ src/component/ShopContainer/ShopContainer.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const ShopContainer = () => {
+const ShopContainer = (props) => {
+    // console.log(props)
+    const {name, img,price, stoke, seller}=props.product
     return (
         <div>
-            
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h2>{name}</h2>
+                <p>only {stoke} left in stock - order soon</p>
+                <p>by {seller}</p>
+                <p>price : {price}</p>
+            </div>
         </div>
     );
 };
